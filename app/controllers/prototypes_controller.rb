3f487b44a8b120094ef0514505d616b84f3b7ffa
hiyokomame2525 <5656mamesiba@gmail.com>
2024-02-11 12:20:50 +0900

    自己訂正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,7 +3,7 @@ class PrototypesController < ApplicationController
   before_action :move_to_index,except: [:index, :show]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
 

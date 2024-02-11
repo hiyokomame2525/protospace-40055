c49e1f907d5d3cf708063e11846bc6003e985fcd
hiyokomame2525 <5656mamesiba@gmail.com>
2024-02-11 17:27:10 +0900

    自己訂正１１

--- db/schema.rb
+++ db/schema.rb
@@ -77,5 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_06_115451) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
